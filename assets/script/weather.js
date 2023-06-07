b9ee7099f0af356249b4d56e91340d5daaa9b285
om kyada <omkyada1i@gmail.com>
2023-06-07 18:40:05 +0530

    design: weather websity style chang | change background image according to weather

--- assets/script/weather.js
+++ assets/script/weather.js
@@ -8,6 +8,24 @@ const getCurrentData = (city) => {
             var sunset = new Date(response.sys.sunset)
             document.querySelector(".current-temperature").innerText = Math.ceil(response.main.temp) + "°C";
             document.querySelector(".current-main").innerText = response.weather[0].main;
+            function backgroundChange(weather) {
+                if (weather == "Rain") {
+                    document.body.style.backgroundImage =
+                        "url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/cac950bd-8f59-4376-8597-52366358d12e/d9wvf1t-b8eec3f5-b88b-4b1f-8aac-36bc21e06a92.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvY2FjOTUwYmQtOGY1OS00Mzc2LTg1OTctNTIzNjYzNThkMTJlXC9kOXd2ZjF0LWI4ZWVjM2Y1LWI4OGItNGIxZi04YWFjLTM2YmMyMWUwNmE5Mi5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.WYDWFRtXw1BslrbVksftWlGdI6xCc0wHMhKMMx-NEuM')";
+                } else if (weather == "Clouds") {
+                    document.body.style.backgroundImage = "url(cloud.gif)"; // give proper url for the image or gif here
+                } else if (weather == "Clear") {
+                    document.body.style.backgroundImage =
+                        "url('https://image.freepik.com/free-photo/panorama-sky-sunrise-sunset-beautiful-view-dark-blue-clouds-lit-by-bright-orange-yellow-sun-clear-sky-beauty-power-nature-meteorology-climate-changing-concept_127089-8097.jpg')";
+                } else if (weather == "Haze") {
+                    document.body.style.backgroundImage =
+                        "url(https://www.pixelstalk.net/wp-content/uploads/2016/06/Dark-Woods-HD-Wallpaper.jpg)";
+                } else {
+                    document.body.style.backgroundImage = "url(background.gif)";  // give proper url for the image or gif here
+                }
+            }
+
+            backgroundChange(document.querySelector(".current-main").innerText);
             document.querySelector(".sunrise-time").innerText = "Sunrise: " + sunrise.toLocaleTimeString();
             document.querySelector(".sunset-time").innerText = "Sunset: " + sunset.toLocaleTimeString();
             document.querySelector(".location").innerText = "Location: " + response.name;
@@ -22,3 +40,10 @@ const getCurrentData = (city) => {
 searchCity.addEventListener("click", () => {
     getCurrentData(enterCity.value);
 })
+enterCity.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        getCurrentData(enterCity.value);
+    }
+    return false;
+});
+
