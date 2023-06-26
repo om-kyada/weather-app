9ae6f25c4f2521ebb8354a0e3a9da6fde3f67dd8
om kyada <omkyada1i@gmail.com>
2023-06-26 12:05:19 +0530

    remove api key

--- assets/script/weather.js
+++ assets/script/weather.js
@@ -1,7 +1,7 @@
 let enterCity = document.getElementById("enter-city");
 let searchCity = document.getElementById("search-city");
 const getCurrentData = (city) => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e02a9a7f8d52c66794a09c113a6baca5&units=metric`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}e02a9a7f8d52c66794a09c113a6baca5&units=metric`)
         .then((response) => response.json())
         .then((response) => {
             var sunrise = new Date(response.sys.sunrise)
